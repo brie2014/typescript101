f34935a3d8e0e79ea240e2692708cb29e15ee2db
Brienna Sambrano <brie2014@byui.edu>
2024-01-27 18:19:28 -0700

    fix fibonacci

--- src/fibonacci/main.js
+++ src/fibonacci/main.js
@@ -1,18 +1,17 @@
 // Recursive function that calculates the given number in the Fibonacci sequence
-function fibonacci(n: number): number {
+function fibonacci(n) {
     // the base case: when n is 0 or 1, return n
     if (n === 0 || n === 1) {
-        return n
+        return n;
     }
     // In the recursive case, calculate the nth number by adding the previous 2 numbers in the sequence
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
-
-function getFibonacci(amount: number = 10) {
+function getFibonacci(amount) {
+    if (amount === void 0) { amount = 10; }
     // Loop to output the specified amount of Fibonacci numbers
-    for (let i = 0; i < amount; i++) {
-        console.log(`Fibonacci of term #${i + 1}: `, fibonacci(i))
+    for (var i = 0; i < amount; i++) {
+        console.log("Fibonacci of term #".concat(i + 1, ": "), fibonacci(i));
     }
 }
-
-getFibonacci()
+getFibonacci();
